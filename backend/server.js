0748bb996807c5daad7148e21dd0d08ae6205f36
Ena <ena.slipicevic@stu.ibu.edu.ba>
2026-01-22 00:40:25 +0100

    fixed url

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 
 
 app.use(cors({
-  origin: 'https://identifyme-app-fnhxi.ondigitalocean.app/', 
+  origin: 'https://identifyme-app-fnhxi.ondigitalocean.app', 
   methods: ['GET', 'POST'],
   credentials: true
 }));
