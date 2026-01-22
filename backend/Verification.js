b73801387ed003d212729dcaee0cb8a34de52223
Ena <ena.slipicevic@stu.ibu.edu.ba>
2026-01-22 21:55:44 +0100

    fixed database problems

--- backend/Verification.js
+++ backend/Verification.js
@@ -2,8 +2,8 @@
 const mongoose = require('mongoose');
 
 const verificationSchema = new mongoose.Schema({
-    dob: { type: String, default: "Not detected" }, 
-    age: { type: Number, default: 0 },
+    dob: { type: String, required: false }, 
+    age: { type: Number, required: false },
     faceConfidence: { type: Number, default: 0 },
     result: { type: String, required: true },
     timestamp: { type: Date, default: Date.now }
