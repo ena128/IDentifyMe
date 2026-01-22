e6e470c83cbe487a2434f37d7f23104cec0e679e
Ena <ena.slipicevic@stu.ibu.edu.ba>
2026-01-22 01:04:22 +0100

    fixed paths

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,8 @@ const Verification = require('./Verification');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const mongoURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/identifyme';
+
 
 
 app.use(cors({
@@ -23,8 +25,8 @@ app.use(cors({
 app.use(express.json());
 
 // MongoDB Connection
-mongoose.connect('mongodb://127.0.0.1:27017/identifyme')
-    .then(() => console.log("Connected to MongoDB (identifyme)..."))
+mongoose.connect(mongoURI)
+    .then(() => console.log("Connected to MongoDB..."))
     .catch(err => console.error("MongoDB connection error:", err));
 
 const upload = multer({ storage: multer.memoryStorage() });
